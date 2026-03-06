609f5e49cad4b5ce720b5738405148f5a5d3bcad
emmanuel <eajejohnson@gmail.com>
2026-03-06 11:34:44 +0100

    feat: added MainSection and Header components; remove Navbar and Section1 components

---
+++ src/Components/Nav/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    
+    <div>
+      <div className='nav'>
+        <h1>Emmanuel</h1>
+      </div>
+      <div className='links'>
+        <a href="#section1">works</a>
+        <a href="#section2">mentality</a>
+        <a href="#section3">contact</a>
+      </div>
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default Header
--- src/Components/Pages/NavPage.jsx
+++ src/Components/Pages/NavPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import Header from '../Nav/Header'
 
 const NavPage = () => {
   return (
     <>
-    
-    <Navbar/>
-    
+    <Header/>
     </>
   )
 }
